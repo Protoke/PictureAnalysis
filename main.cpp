44b44de2d01be76397c758c53dff4bd25c8d52a3
DecolleAnais <anais_decolle@orange.fr>
2017-09-23 15:34:47 +0200

    suppression images tests gradients 10*10 ajout images tests gradients 200*200 creation classe window, gestion de SDL2 conversion possible d'une cv::mat vers une texture SDL a afficher gestion des events clavier

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <SDL2/SDL.h>
+#include "window.h"
+
+using namespace cv;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    Window window("Picture Analysis", 800, 600);
+
+    // load the original picture in cv mat
+    Mat img;
+    img = imread("../data/horizontal.png", CV_LOAD_IMAGE_COLOR);
+
+    if(! img.data) {
+        std::cout << "Error loading picture" << std::endl;
+        return -1;
+    }
+
+    // load the cv mat in the window
+    window.fill_texture_from_CVmat(img);
+
+    // main loop
+    while(window.events()) {
+        // key events
+        if(window.key_state(SDLK_ESCAPE))
+            break;
+
+        // display
+        window.render();
+    }
+
+    // exit
+    window.close();
+
+
+    return EXIT_SUCCESS;
+
 }
