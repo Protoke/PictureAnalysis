26fd8a1fde57392e4c76634c2821f028c9793767
Protoke <auffret.dev@gmail.com>
2017-10-20 22:33:02 +0200

    Interface

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <string.h>
 
 using namespace cv;
+using namespace std;
 
 Mat mat2gray(const Mat& src){
     Mat dst;
@@ -16,25 +17,14 @@ Mat mat2gray(const Mat& src){
     return dst;
 }
 
-std::vector< std::list<Point2i> > chaining(const Mat& affine, const Mat& orientation) {
-    std::vector<std::list<Point2i> > chains;
-
-    return chains;
-
-}
-
-void computeContours(const Mat& image, int blurSize,
+void computeContours(const Mat& image, Mat& result,
+                     Gradient::type filterType, int gradientType,
                      float thresholdHigh, float thresoldLow, int thresholdSize,
                      int refiningSize,
-                     bool showIntermediate = false){
-
-    GaussianBlur(image, image, Size(blurSize, blurSize), 0, 0);
-    if(showIntermediate)
-        imshow("Blur", image);
+                     bool showIntermediate){
 
     // calcul gradients, magnitude, orientation de l'image
-//    Gradient gradient(image, 3, Gradient::PREWITT, Gradient::E | Gradient::S | Gradient::SW | Gradient::SE);
-    Gradient gradient(image, 3, Gradient::PREWITT, Gradient::E | Gradient::N);
+    Gradient gradient(image, 3, filterType, gradientType);
 
     // affichage des gradients
 //    for(unsigned int i = 0;i < gradient._gradients.size();i++) {
@@ -46,7 +36,7 @@ void computeContours(const Mat& image, int blurSize,
     if(showIntermediate) {
         imshow("Magnitude", mat2gray(gradient._magnitude));
         imshow("Orientation", mat2gray(gradient._orientation));
-        imshow("Orientation Map", gradient._orientation_map);
+//        imshow("Orientation Map", gradient._orientation_map);
     }
 
     Mat seuil = hysteresisThreshold(gradient._magnitude, thresholdHigh, thresoldLow, thresholdSize);
@@ -56,26 +46,99 @@ void computeContours(const Mat& image, int blurSize,
     Mat affine;
     refineContour(gradient._magnitude, gradient._orientation, seuil, affine, refiningSize);
     if(showIntermediate)
-        imshow("Affinage", affine);
+        imshow("Affinée", affine);
+
+    result = affine;
 }
 
 int main() {
 
-    // load the original picture in cv mat
+    cout << "Entrez le chemin de l'image : " << endl;
+    string path;
+    getline(cin, path);
     Mat image;
-//    image = imread("../data/Lenna.png", CV_LOAD_IMAGE_COLOR);
-    image = imread("../data/image_simple_test.png", CV_LOAD_IMAGE_COLOR);
+    image = imread(path, CV_LOAD_IMAGE_COLOR);
+    if(! image.data) {
+        std::cout << "Erreur de chargement d'image" << std::endl;
+        return -1;
+    }
+    imshow("Source", image);
+
+    cout << "Entrez le type du filtre : " << endl;
+    cout << "(1 : Prewitt, 2 : SOBEL, 3 : KIRSCH)" << endl;
+    int in;
+    Gradient::type filterType;
+    cin >> in;
+    switch(in){
+        case 1:
+            filterType = Gradient::PREWITT;
+            break;
+        case 2:
+            filterType = Gradient::SOBEL;
+            break;
+        case 3:
+            filterType = Gradient::KIRSCH;
+            break;
+        default:
+            cout << "Valeur incorrecte" << endl;
+            return -1;
+    }
+
+    cout << "Entrez le type de gradient : " << endl;
+    cout << "(1 : Bidirectionnel, 2 : Multidirectionnel)" << endl;
+    int gradientFlags;
+    cin >> in;
+    switch(in){
+        case 1:
+            gradientFlags = Gradient::N | Gradient::E;
+            break;
+        case 2:
+            gradientFlags = Gradient::NW | Gradient::N | Gradient::NE | Gradient::E;
+            break;
+        default:
+            cout << "Valeur incorrecte" << endl;
+            return -1;
+    }
+
+    cout << "Entrez la valeur haute de seuillage : ";
+    int thresholdMax;
+    cin >> thresholdMax;
+
+    cout << "Entrez la valeur basse du seuillage : ";
+    int thresholdMin;
+    cin >> thresholdMin;
+
+    cout << "Entrez la largeur du filtre de seuillage : ";
+    int thresholdSize;
+    cin >> thresholdSize;
+
+    cout << "Entrez la largeur du filtre d'affinage : ";
+    int refiningSize;
+    cin >> refiningSize;
+
+    cout << "Voulez-vous afficher les images intermédiaires? (0 = non, 1 = oui) : ";
+    int showIntermediate;
+    cin >> showIntermediate;
+
+    Mat result;
+    computeContours(image, result, filterType, gradientFlags,
+                    thresholdMax, thresholdMin, thresholdSize, refiningSize, showIntermediate);
+
+
+    /*// load the original picture in cv mat
+    Mat image = imread("../data/Lenna.png", CV_LOAD_IMAGE_COLOR);
+//    image = imread("../data/image_simple_test.png", CV_LOAD_IMAGE_COLOR);
     if(! image.data) {
         std::cout << "Error loading picture" << std::endl;
         return -1;
     }
     imshow("Source", image);
 
-    computeContours(image, 3, 50, 25, 3, 3, true);
-//    computeContours(image, 3, 100, 75, 3, 3, true);
+    Mat result;
+    computeContours(image, result, Gradient::PREWITT, Gradient::N | Gradient::E, 50, 25, 3, 3, 1);*/
 
+    imshow("Resultat", result);
     waitKey(0);
-
 
     return EXIT_SUCCESS;
 }
